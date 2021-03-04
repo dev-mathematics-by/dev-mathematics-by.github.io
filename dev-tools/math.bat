a0dfd33726888e94355b241a59a07c0a5290759c
shadow-reviewer <shadow.reviever.review@gmail.com>
2021-03-04 08:39:11 +0300

    M-84. Creation of necessary folder to copy into.

--- dev-tools/math.bat
+++ dev-tools/math.bat
@@ -1,3 +1,6 @@
 echo "Hello World!"
-copy "D:\sites\mathematics_by\dev-mathematics-by.github.io\images\photo\tutor.jpg" "D:\sites\mathematics_by\Release-1.1\FromScript\"
+cd "D:\sites\mathematics_by\Release-1.1\FromScript\"
+mkdir images\photo
+copy "D:\sites\mathematics_by\dev-mathematics-by.github.io\images\photo\tutor.jpg" "D:\sites\mathematics_by\Release-1.1\FromScript\images\photo\"
+cd "D:\sites\mathematics_by\dev-mathematics-by.github.io\dev-tools\"
 REM pause
