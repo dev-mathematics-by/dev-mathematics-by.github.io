7f70054cc38f2d496836b0c922bd00cbec5f953b
shadow-reviewer <shadow.reviever.review@gmail.com>
2021-03-04 08:42:37 +0300

    M-84. Copy the second file

--- dev-tools/math.bat
+++ dev-tools/math.bat
@@ -2,5 +2,6 @@ echo "Hello World!"
 cd "D:\sites\mathematics_by\Release-1.1\FromScript\"
 mkdir images\photo
 copy "D:\sites\mathematics_by\dev-mathematics-by.github.io\images\photo\tutor.jpg" "D:\sites\mathematics_by\Release-1.1\FromScript\images\photo\"
+copy "D:\sites\mathematics_by\dev-mathematics-by.github.io\images\photo\mathematics_tutor.jpg" "D:\sites\mathematics_by\Release-1.1\FromScript\images\photo\"
 cd "D:\sites\mathematics_by\dev-mathematics-by.github.io\dev-tools\"
 REM pause
